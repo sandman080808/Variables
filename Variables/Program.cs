b6d5ffe36e698a4d65a184f4ea4ea875d84925bf
sandman080808 <dr6339437@gmail.com>
2024-10-09 21:23:51 +0300

    Add files via upload

---
+++ Variables/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int rusDamage;
+            int rusHealth = 250;
+            int rusDodge;
+
+            int lizardDodge;
+            int lizardDamage;
+            int lizardHealth = 50;
+            int lizardQuantity = 8;
+
+            while (rusHealth > 0 && lizardQuantity > -1)
+            {
+                Console.WriteLine("---- Ход Ящера ----");
+
+                rusDodge = rnd.Next(1, 100);
+
+                if (rusDodge >= 60)
+                {
+                    lizardDamage = rnd.Next(1, 40);
+                    rusHealth -= lizardDamage;
+                    Console.WriteLine($"Ящер нанёс вам {lizardDamage} урона и у вас осталось {rusHealth} здоровья!");
+                }
+                else
+                {
+                    Console.WriteLine("Вы увернулись");
+                }
+
+                Console.WriteLine("---- Ход Салошки ----");
+                Console.WriteLine("Здоровье: {0}", rusHealth);
+
+                lizardDodge = rnd.Next(1, 100);
+
+
+                if (lizardDodge >= 30)
+                {
+                    rusDamage = rnd.Next(1, 60);
+                    lizardHealth -= rusDamage;
+                    Console.WriteLine($"Вы нанесли ящеру {rusDamage} урона и у него осталось {lizardHealth} здоровья!");
+                }
+                else
+                {
+                    Console.WriteLine("Вы промахнулись");
+                }
+
+                if (lizardHealth <= 0)
+                {
+                    lizardHealth = rnd.Next(20, 90);
+                    lizardQuantity--;
+                    if (lizardQuantity <= -1)
+                    {
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("На сцену выходит новый ящер! " + lizardQuantity + " ящеров осталось");
+                    }
+                }
+            }
+            if (rusHealth > 0)
+            {
+                Console.WriteLine($"Салошник победил и у него осталось {rusHealth} здоровья!");
+            }
+            else
+            {
+                Console.WriteLine($"Салошник проиграл а ведь ему осталось победить всего {lizardQuantity} ящеров");
+            }
+        }
+    }
+}
---
+++ Variables/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Variables")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Variables")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("eb478cc9-6b0f-4062-95d6-c53e708ea730")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
